7ac050ad7e84c53ac7ad9c386df5362f39c35544
gammafunk <gammafunk@gmail.com>
2015-05-22 13:26:02 -0500

    Add argument to run a specific objstat job and refactor
    Adds -j to objstat.sh so that a specific job can be run by directory
    name. Refactor the script to use bash associative arrays for the job
    directories and level strings.

--- objstat.sh
+++ objstat.sh
@@ -6,40 +6,64 @@
 
 NUM_ITERS=5
 CRAWL_DIR=.
-while getopts "h?c:n:" opt; do
+declare -A JOBS
+JOBS["3-rune-swamp-snake"]="D,Temple,Sewer,Ossuary,Orc,Elf,Bailey,Lair, \
+                            Swamp,Snake,IceCv,Volcano,Lab,Trove,Vaults, \
+                            Crypt,Bazaar,WizLab,Depths,Zot"
+JOBS["3-rune-shoals-spider"]="D,Temple,Sewer,Ossuary,Orc,Elf,Bailey,Lair,\
+                              Shoals,Spider,IceCv,Volcano,Lab,Trove, \
+                              Vaults,Crypt,Bazaar,WizLab,Depths,Zot"
+JOBS["extended-no-abyss-pan-zig"]="D,Temple,Sewer,Ossuary,Orc,Elf,Bailey, \
+                                   Lair,Swamp,Snake,Slime,IceCv,Volcano, \
+                                   Lab,Trove,Vaults,Crypt,Tomb,Bazaar, \
+                                   WizLab,Depths,Zot,Hell,Geh,Coc,Dis,Tar"
+JOBS["abyss-pan-zig"]="Abyss,Pan,Zig"
+JOB=
+
+function do_help() {
+    echo "$0 [-c <crawl-dir> ] [-j <job-name>] [-n <num>]"
+}
+
+while getopts "h?c:n:j:" opt; do
     case "$opt" in
         h|\?)
-            echo "$0 [-c <crawl-dir> ] [-n <num>]"
+            do_help
             exit 0
             ;;
         c)  CRAWL_DIR="$OPTARG"
+            ;;
+        j)  
+            if [ -z "${JOBS[$OPTARG]}" ]; then
+                echo "Job name must be one of: ${!JOBS[@]}"
+                exit 1
+            else
+                JOB="$OPTARG"
+            fi
             ;;
         n)  NUM_ITERS="$OPTARG"
             ;;
     esac
 done
 
-SWSN_DIR=3-rune-swamp-snake
-SWSN_LEV="D,Temple,Sewer,Ossuary,Orc,Elf,Bailey,Lair,Swamp,Snake,IceCv,Volcano,Lab,Trove,Vaults,Crypt,Bazaar,WizLab,Depths,Zot"
-
-SHSP_DIR=3-rune-shoals-spider
-SHSP_LEV="D,Temple,Sewer,Ossuary,Orc,Elf,Bailey,Lair,Shoals,Spider,IceCv,Volcano,Lab,Trove,Vaults,Crypt,Bazaar,WizLab,Depths,Zot"
-
-EXT_DIR=extended-no-abyss-pan-zig
-EXT_LEV="D,Temple,Sewer,Ossuary,Orc,Elf,Bailey,Lair,Swamp,Snake,Slime,IceCv,Volcano,Lab,Trove,Vaults,Crypt,Tomb,Bazaar,WizLab,Depths,Zot,Hell,Geh,Coc,Dis,Tar"
-
-APZ_DIR=abyss-pan-zig
-APZ_LEV="Abyss,Pan,Zig"
 
 tldir=`cd "$CRAWL_DIR" && git rev-parse --show-toplevel`
 SDIR="$tldir/crawl-ref/source"
 # 24-hour timeout since a single job can take many hours
 gcc -DTIMEOUT=1440 "$SDIR"/util/fake_pty.c -o fake_pty -lutil
 
-parallel --ungroup <<EOF
-./objstat-job.sh -c $CRAWL_DIR -d $APZ_DIR -l $APZ_LEV -n $NUM_ITERS -r
-./objstat-job.sh -c $CRAWL_DIR -d $SWSN_DIR -l $SWSN_LEV -n $NUM_ITERS 
-./objstat-job.sh -c $CRAWL_DIR -d $SHSP_DIR -l $SHSP_LEV -n $NUM_ITERS
-./objstat-job.sh -c $CRAWL_DIR -d $EXT_DIR -l $EXT_LEV -n $NUM_ITERS
+if [ -n "$JOB" ]; then
+    JOB_ARGS=
+    if [ "$JOB" = "abyss-pan-zig" ]; then
+        JOB_ARGS="-r"
+    fi
+    ./objstat-job.sh -c "$CRAWL_DIR" -d "$JOB" -l "${JOBS[$JOB]}" -n "$NUM_ITERS" "$JOB_ARGS"
+else
+    parallel --ungroup <<EOF
+./objstat-job.sh -c "$CRAWL_DIR" -d 3-rune-swamp-snake -l "${JOBS[3-rune-swamp-snake]}" -n "$NUM_ITERS"
+./objstat-job.sh -c "$CRAWL_DIR" -d 3-rune-shoals-spider -l "${JOBS[3-rune-shoals-spider]}" -n "$NUM_ITERS"
+./objstat-job.sh -c "$CRAWL_DIR" -d extended-no-abyss-pan-zig -l "${JOBS[extended-no-abyss-pan-zig]}" -n "$NUM_ITERS"
+./objstat-job.sh -c "$CRAWL_DIR" -d abyss-pan-zig -l "${JOBS[abyss-pan-zig]}" -n "$NUM_ITERS" -r
 EOF
+
+fi
 
